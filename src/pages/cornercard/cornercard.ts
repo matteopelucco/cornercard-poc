553216c29ec18f33546eed82c5eb64d47b832908
Matteo Pelucco <matteo.pelucco@gmail.com>
2018-03-08 16:07:51 +0100

    complete i18n

--- src/pages/cornercard/cornercard.ts
+++ src/pages/cornercard/cornercard.ts
@@ -8,7 +8,7 @@ import { NavController } from 'ionic-angular';
 export class CornercardPage {
 
   constructor(public navCtrl: NavController) {
-
+    
   }
 
 }
--- src/pages/emotions/emotions.ts
+++ src/pages/emotions/emotions.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'page-emotions',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class EmotionsPage {
 
-  constructor(public navCtrl: NavController) {
-
+  emotionItems: any[];
+ 
+  constructor(public navCtrl: NavController, translate: TranslateService) {
+    this.emotionItems = [];
+    for (let i = 1; i <= 4; i++) {
+      this.emotionItems.push({
+        img: 'assets/img/emotions/emotion' + i + '.jpg', 
+        title: translate.get("emotions.post." + i +".title")['value'], 
+        subtitle: translate.get("emotions.post." + i +".subtitle")['value'], 
+        badge: translate.get("emotions.post." + i +".badge")['value']
+      });
+    }
   }
-
 }
